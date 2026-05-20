fe1daa9c5cace6f98071e4bac14eb50f23951978
Tulaib Ahmad <tulaibahmad996@gmail.com>
2026-05-20 12:16:53 +0500

    fix: use env database url

--- backend/src/config/datasource.js
+++ backend/src/config/datasource.js
@@ -5,7 +5,7 @@ const { DataSource } = require('typeorm');
 
 const dataSource = new DataSource({
   type: 'postgres',
-  host: process.env.DATABASE_HOST || '127.0.0.1',
+  url: process.env.DATABASE_HOST || '127.0.0.1',
   port: parseInt(process.env.DATABASE_PORT || '5432', 10),
   username: process.env.DATABASE_USER || 'postgres',
   password: process.env.DATABASE_PASSWORD || 'postgres',
