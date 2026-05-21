11b2950b7d2a269ffbf9fb5f3fff618d410fb02e
Tulaib Ahmad <tulaibahmad996@gmail.com>
2026-05-22 00:48:21 +0500

    use json

--- backend/server.js
+++ backend/server.js
@@ -55,7 +55,7 @@ const cors = require('cors');
 const path = require('path');
 
 const dataSource = require('./src/config/datasource');
-
+app.use(express.json());
 // Routes
 const authRoutes = require('./src/routes/authRoutes');
 const userRoutes = require('./src/routes/userRoutes');
